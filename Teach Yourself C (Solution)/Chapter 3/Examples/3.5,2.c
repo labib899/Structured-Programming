48f80f6032c9e63f4304ba05246663a652cdf51c
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2024-02-21 21:43:51 +0600

    Add files via upload

---
+++ Teach Yourself C (Solution)/Chapter 3/Examples/3.5,2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    do
+    {
+        ch=getchar();
+    }while(ch!='q');
+
+    printf("Found the q.");
+
+    return 0;
+}
+
---
+++ Teach Yourself C (Solution)/Chapter 4/Exercise/4.1,2.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+
+int main()
+{
+    unsigned long int a,d;
+    printf("Input a distence");
+    scanf("%lu",&a);
+    d = a/186000;
+    printf("time is %d",d);
+    
+    return 0;
+}
